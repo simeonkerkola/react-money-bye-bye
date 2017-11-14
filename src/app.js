7792553d00ee626d0655c3039beded2b53310327
sssmi <simeon.kerkola@gmail.com>
2017-11-14 11:24:31 +0200

    Add moment mocking library and test ExpenseForm

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
