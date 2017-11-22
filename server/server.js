3ebac6c9f8375b76e4d282e5aa43ceb1012eccfe
sssmi <simeon.kerkola@gmail.com>
2017-11-22 16:57:23 +0200

    Setup Production Webpac build 2/2, Separate CSS files

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const publicPath = path.join(__dirname, '..', 'public') // current, go back, go public
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html')) // serve index.html every time we get a 404
+})
+
+app.listen(3000, () => {
+  console.log('Running on a port', port);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = (env) => {
   const isProduction = env === 'production'
+  const CSSExtract = new ExtractTextPlugin('styles.css')
 
   return {
     entry: './src/app.js',
@@ -18,19 +20,32 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/, // look for both css and scss files
-          use: [
-            // use allows us to use an array of loaders
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
+          // use allows us to use an array of loaders
+          use: CSSExtract.extract({
+            // loaders go inside a plugin
+            use: [{
+              loader: 'css-loader',
+              options: {
+                sourceMap: true, // enable sourcemap support for more dev friendly environment
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              }
+            }],
+          }),
         },
       ],
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', // check if production, implement sourcemap
+    plugins: [
+      // plugins go here
+      CSSExtract,
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map', // check if production, implement sourcemap
     devServer: {
       contentBase: path.join(__dirname, 'public'), // watch public folder
-      historyApiFallback: true, // tells dev-server we r handling routing via client side code
+      historyApiFallback: true, // tells dev-server we r handling routing via client side code (serve index.html every time we get a 404)
     },
   }
 }
