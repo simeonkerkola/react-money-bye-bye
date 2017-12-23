9024adee2b035cd6f1d673a2acb3364549005c40
sssmi <simeon.kerkola@gmail.com>
2017-12-23 20:45:03 +0200

    Add Public only Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,10 @@ import { startLogout } from '../actions/auth'
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
+    {/* <NavLink to="/" activeClassName="is-active" exact>Home</NavLink> */}
     <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
 )
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,6 +8,7 @@ import LoginPage from '../components/LoginPage'
 import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory()
 
@@ -16,7 +17,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact />
+        <PublicRoute path="/" component={LoginPage} exact />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
