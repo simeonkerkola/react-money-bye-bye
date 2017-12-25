069b7239756240e90a1c8782dbe0f04cbc2d67b0
sssmi <simeon.kerkola@gmail.com>
2017-12-25 10:51:45 +0200

    Style Login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux'
 import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <p>Login please</p>
-    <button onClick={startLogin}>Log in</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify App</h1>
+      <p>Get your expenses under control</p>
+      <button onClick={startLogin}>Login</button>
+      <button>Try Out</button>
+    </div>
   </div>
 )
 
